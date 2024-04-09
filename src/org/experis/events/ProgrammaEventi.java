03343d16cb81580dfd246a61fc7eaa6562fbf1c3
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-04-09 14:50:26 +0200

    Init ProgrammaEventi with List Evento

---
+++ src/org/experis/events/ProgrammaEventi.java
@@ -0,0 +1,11 @@
+package org.experis.events;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProgrammaEventi {
+    //attributi
+    private String titolo;
+    List<Evento> eventi = new ArrayList<>();
+
+}

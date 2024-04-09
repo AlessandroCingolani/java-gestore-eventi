614aa66b47cbcb4f220b6fff67818be4da5a7daf
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-04-09 16:45:05 +0200

    Add methods filter stream and size event array

--- src/org/experis/events/MainProgrammaEventi.java
+++ src/org/experis/events/MainProgrammaEventi.java
@@ -7,12 +7,23 @@ import java.util.List;
 
 public class MainProgrammaEventi {
     public static void main(String[] args) {
+        ProgrammaEventi list = null;
         try {
-            ProgrammaEventi list = new ProgrammaEventi("Ciao");
+            list = new ProgrammaEventi("Ciao");
             list.addEvento(new Evento("ciao", LocalDate.parse("2024-11-12"),1000));
+            list.addEvento(new Evento("bella", LocalDate.parse("2024-11-11"),2000));
+            list.addEvento(new Evento("asdrubbale", LocalDate.parse("2024-11-12"),2000));
             System.out.println(list);
         } catch (EventoException e) {
             System.out.println(e);
         }
+        // filtraggio per data
+        System.out.println(list.filterData( LocalDate.parse("2024-11-12")));
+
+        // size array eventi
+        System.out.println(list.arraySize());
+
+        // svuoto array eventi
+
     }
 }
--- src/org/experis/events/ProgrammaEventi.java
+++ src/org/experis/events/ProgrammaEventi.java
@@ -1,12 +1,14 @@
 package org.experis.events;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ProgrammaEventi {
     //attributi
     private String titolo;
-    List<Evento> eventi;
+    private List<Evento> eventi;
 
     // costruttore
 
@@ -29,7 +31,13 @@ public class ProgrammaEventi {
        eventi.add(evento);
     }
 
-//    public List<Evento> filterData(List eventi)
+    public List<Evento> filterData(LocalDate data) {
+        return this.eventi.stream().filter(e -> e.getData().equals(data)).collect(Collectors.toList());
+    }
+
+    public Integer arraySize(){
+       return this.eventi.size();
+    }
 
     @Override
     public String toString() {
@@ -38,4 +46,5 @@ public class ProgrammaEventi {
                 ", eventi=" + eventi +
                 '}';
     }
+
 }

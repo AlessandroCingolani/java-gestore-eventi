fc9122cc192ceb128394c0a95af42a8d02f56af7
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-04-09 10:14:28 +0200

    Add methods and getter setter

--- src/org/experis/events/Evento.java
+++ src/org/experis/events/Evento.java
@@ -1,17 +1,31 @@
 package org.experis.events;
 
-import java.time.LocalDate;
+import exceptions.EventoException;
 
-public abstract class Evento {
-    // attributi
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public  class Evento {
+    // Attributi
     private String titolo;
     private LocalDate data;
     private int numeroPostiTotali;
     private int numeroPostiPrenotati;
 
 
-    // costruttori
-    public Evento(String titolo, LocalDate data, int numeroPostiTotali) {
+    // Costruttori
+    public Evento(String titolo, LocalDate data, int numeroPostiTotali) throws EventoException {
+        if(titolo == null || titolo.isEmpty()) {
+            throw new EventoException("Il titolo non può essere nullo o vuoto!");
+        }
+        if (data == null || data.isBefore(LocalDate.now())){
+            throw new EventoException("La data non può essere nulla o minore della data attuale!");
+        }
+        if(numeroPostiTotali <= 0){
+            throw new EventoException("Il numero dei posti totali deve essere maggiore di 0!");
+        }
+
         this.titolo = titolo;
         this.data = data;
         this.numeroPostiTotali = numeroPostiTotali;
@@ -20,8 +34,57 @@ public abstract class Evento {
 
 
 
-    // getter e setter
+    // Getter e Setter
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) throws EventoException{
+        if(titolo == null || titolo.isEmpty()) {
+            throw new EventoException("Il titolo non può essere nullo o vuoto!");
+        }
+        this.titolo = titolo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) throws EventoException {
+        if (data == null || data.isBefore(LocalDate.now())){
+            throw new EventoException("La data non può essere nulla o minore della data attuale!");
+        }
+        this.data = data;
+    }
+
+    public int getNumeroPostiTotali() {
+        return numeroPostiTotali;
+    }
 
 
-    // metodi
+    public int getNumeroPostiPrenotati() {
+        return numeroPostiPrenotati;
+    }
+
+
+    // Metodi
+//    public void prenota(int numeroPostiPrenotati){
+//        if()
+//
+//    }
+//
+//    public void  disdici(){
+//
+//    }
+
+    private String formattaData(LocalDate data){
+        DateTimeFormatter formatoItaliano = DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.ITALIAN);
+        return data.format(formatoItaliano);
+    }
+
+    @Override
+    public String toString() {
+        return formattaData(getData());
+    }
 }
